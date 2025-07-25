a49e2a5522a7e9a171ba8243ff6dbb26a78d63ad
Ademun <78663343+Ademun@users.noreply.github.com>
2025-07-26 01:15:07 +0300

    refactoring

--- network/scanner.go
+++ network/scanner.go
@@ -1,5 +1,13 @@
 package network
 
+import (
+	"context"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
 type Scanner interface {
 	ScanHost(string, []string) []Result
 }
@@ -34,4 +42,69 @@ type Result struct {
 	Port    string
 	Status  PortStatus
 	Banners string
+}
+
+func ScanHost(target string, ports []string) []Result {
+	results := make([]Result, 0, len(ports))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	semaphore := make(chan struct{}, 100)
+	out := make(chan Result)
+	wg := &sync.WaitGroup{}
+
+	wg.Add(len(ports))
+	for _, p := range ports {
+		go func(port string) {
+			defer wg.Done()
+			semaphore <- struct{}{}
+			scanConn(ctx, out, "tcp", target, port)
+			<-semaphore
+		}(p)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	for r := range out {
+		results = append(results, r)
+	}
+	return results
+}
+
+func scanConn(ctx context.Context, out chan Result, protocol string, target string, port string) {
+	switch protocol {
+	case "tcp":
+		tcpScan(ctx, out, target, port)
+	}
+}
+
+func tcpScan(ctx context.Context, out chan Result, target string, port string) {
+	address := net.JoinHostPort(target, port)
+	conn, err := net.DialTimeout("tcp", address, time.Second*10)
+	select {
+	case <-ctx.Done():
+		return
+	default:
+		if err != nil {
+			if isConnectionRefused(err) {
+				out <- Result{Port: port, Status: CLOSED}
+				return
+			}
+			out <- Result{Port: port, Status: FILTERED}
+			return
+		}
+	}
+	defer conn.Close()
+	service := DetectService(conn, port)
+	select {
+	case <-ctx.Done():
+		return
+	case out <- Result{Port: port, Status: OPEN, Banners: service.Name}:
+	}
+}
+
+func isConnectionRefused(err error) bool {
+	return strings.Contains(err.Error(), "connectex")
 }
